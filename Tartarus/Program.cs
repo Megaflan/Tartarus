1b3c72c466fb37e2a503d4f5f8e66eb5105a3256
Megaflan Dull <megaflan@outlook.es>
2018-01-01 23:23:18 +0100

    Vectors added to the process

--- Tartarus/Program.cs
+++ Tartarus/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Tartarus
@@ -13,25 +14,46 @@ namespace Tartarus
         {
             List<byte> endian1 = new List<byte>();
             List<byte> endian2 = new List<byte>();
-            byte[] e2 = new Byte[endian2.Count];
+            int numParam = Int32.Parse(args[1])*(-1);
+            byte[] e = new Byte[numParam];
+            var state = File.ReadAllBytes(args[2]);
             switch (args[0])
             {
                 case "-f":
-                    endian1 = File.ReadAllBytes(args[1]).ToList();
-                    endian2 = endian1.ToList();
-                    endian2.Reverse();
-                    e2 = endian2.ToArray();
-                    File.WriteAllBytes(args[1] + ".conv", e2);
+                    using (BinaryReader br = new BinaryReader(File.Open(args[2], FileMode.Open)))
+                    {
+                        br.BaseStream.Position = 0;
+                        for (int i = 0; i < state.Length/numParam; i++)
+                        {
+                            endian1 = br.ReadBytes(numParam).ToList();
+                            endian1.Reverse();
+                            foreach (var item in endian1)
+                            {
+                                endian2.Add(item);
+                            }
+                        }
+                        e = endian2.ToArray();
+                        File.WriteAllBytes(args[2] + ".conv", e);
+                    }
                     break;
                 case "-d":
-                    foreach (var filefound in Directory.GetFiles(args[1]))
+                    foreach (var filefound in Directory.GetFiles(args[2]))
                     {
-                        
-                        endian1 = File.ReadAllBytes(args[1]).ToList();
-                        endian2 = endian1.ToList();
-                        endian2.Reverse();
-                        e2 = endian2.ToArray();
-                        File.WriteAllBytes(args[1] + ".conv", e2);
+                        using (BinaryReader br = new BinaryReader(File.Open(filefound, FileMode.Open)))
+                        {
+                            br.BaseStream.Position = 0;
+                            for (int i = 0; i < state.Length / numParam; i++)
+                            {
+                                endian1 = br.ReadBytes(numParam).ToList();
+                                endian1.Reverse();
+                                foreach (var item in endian1)
+                                {
+                                    endian2.Add(item);
+                                }
+                            }
+                            e = endian2.ToArray();
+                            File.WriteAllBytes(args[2] + ".conv", e);
+                        }
                     }
                     break;
 
