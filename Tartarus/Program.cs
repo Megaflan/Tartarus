9f6ecae4672fd535fafc3b9de8b383a0387d00f0
Megaflan Dull <megaflan@outlook.es>
2018-01-01 18:18:33 +0100

    Tartarus finished

---
+++ Tartarus/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Tartarus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<byte> endian1 = new List<byte>();
+            List<byte> endian2 = new List<byte>();
+            byte[] e2 = new Byte[endian2.Count];
+            switch (args[0])
+            {
+                case "-f":
+                    endian1 = File.ReadAllBytes(args[1]).ToList();
+                    endian2 = endian1.ToList();
+                    endian2.Reverse();
+                    e2 = endian2.ToArray();
+                    File.WriteAllBytes(args[1] + ".conv", e2);
+                    break;
+                case "-d":
+                    foreach (var filefound in Directory.GetFiles(args[1]))
+                    {
+                        
+                        endian1 = File.ReadAllBytes(args[1]).ToList();
+                        endian2 = endian1.ToList();
+                        endian2.Reverse();
+                        e2 = endian2.ToArray();
+                        File.WriteAllBytes(args[1] + ".conv", e2);
+                    }
+                    break;
+
+            }
+            
+               
+        }
+    }
+}
